7d026af6fefc848bc4303f058e90bef039c4a07a
Andrei Kudrin <andreikudrin@gmail.com>
2020-07-05 17:37:17 +0100

    Data Structures and Algo -  Balanced Parentheses in Expression using Stack added new method

--- Data Structures/Stack/BalancedBracketsStack.java
+++ Data Structures/Stack/BalancedBracketsStack.java
@@ -41,9 +41,37 @@ public class BalancedBracketsStack {
 
     }
 
+    public static boolean balancedParenthensies(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
     public static void main(String[] args) {
         System.out.println(isBalanced("(){}[]"));
         System.out.println(isBalanced("()[{}"));
+        System.out.println("---------------------------");
+        System.out.println(balancedParenthensies("{(a,b)}"));
+        System.out.println(balancedParenthensies("{(a},b)"));
+        System.out.println(balancedParenthensies("{)(a,b}"));
 
     }
 
