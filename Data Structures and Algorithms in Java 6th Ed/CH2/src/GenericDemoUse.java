2f227343dbe2e2a4598fcc8af2313c20fda0ecbd
Andrei Kudrin <andreikudrin@gmail.com>
2021-04-25 11:23:24 +0100

    DS&A 6th - GenericDemo Reverse Array

---
+++ Data Structures and Algorithms in Java 6th Ed/CH2/src/GenericDemoUse.java
@@ -0,0 +1,12 @@
+
+public class GenericDemoUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GenericDemo demo = new GenericDemo();
+		String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+		demo.reverse(cars);
+		System.out.println(cars[0]);
+	}
+
+}
